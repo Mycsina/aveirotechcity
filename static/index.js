b5e558c7eefe6394d8b3a0eba4ce189bd4ca768a
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2023-10-04 16:48:24 +0100

    image change

--- static/index.js
+++ static/index.js
@@ -1,20 +1,27 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if(urlParams.has('id')) {
+    const id = urlParams.get('id');
+    openSidebar(id);
+}
+
 function closeDiv(id) {
     let obj = String(id);
     document.getElementById(obj).style.display = "none";
 };
 
 function openSidebar(id) {
+    document.getElementById('main').style.display = "flex";
     document.getElementById('sidebar').style.display = "flex";
-    //fetch imagem google
+    fetch('http://127.0.0.1:9092/get_place_photo')
+        .then(response => response.json())
+        .finally(() => {
+            document.getElementById('locationImg').src = response;
+        });
 };
 
 function openModal(id) {
     document.getElementById('graphModal').style.display = "flex";
     //fetch gráfico
 };
-
-window.addEventListener("popstate", (event) => {
-    console.log(
-        `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
-    );
-});
