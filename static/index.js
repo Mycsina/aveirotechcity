000f2d49fa3bee22f113d5509224b4c6fe8c746c
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2023-10-04 15:03:35 +0100

    updates

--- static/index.js
+++ static/index.js
@@ -1,5 +1,20 @@
 function closeDiv(id) {
     let obj = String(id);
-    console.log(obj)
     document.getElementById(obj).style.display = "none";
 };
+
+function openSidebar(id) {
+    document.getElementById('sidebar').style.display = "flex";
+    //fetch imagem google
+};
+
+function openModal(id) {
+    document.getElementById('graphModal').style.display = "flex";
+    //fetch gráfico
+};
+
+window.addEventListener("popstate", (event) => {
+    console.log(
+        `location: ${document.location}, state: ${JSON.stringify(event.state)}`,
+    );
+});
